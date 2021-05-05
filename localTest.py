94ef26b7663c1ccfbb4b2d1a65e0f591474dfc9a
Devin Bailey <60447242+mouzing@users.noreply.github.com>
2021-05-04 23:02:57 -0400

    Update localTest.py

--- localTest.py
+++ localTest.py
@@ -1,27 +1,27 @@
 from testGame import testGame
 from FSM import *
+from BotS import BattleOfTheSexes
+from TravDem import TravelersDilemma
 import random
 import copy
-import sys
 
 lower_num_games = 25
 upper_num_games = 25
 # number of rounds for each game for scoring for upcoming generation
 num_rounds = random.randint(lower_num_games, upper_num_games) 
+noise = False
 
-# for x in range(18):
-# 	print(fsm.node_list[x].transition_dict)
+
+#game = testGame()
+#game = BattleOfTheSexes()
+game = TravelersDilemma()
+
 
 def make_child(fsm):
 	child_fsm = copy.deepcopy(fsm)
-	if id(child_fsm) == id(fsm):
-		print("fsm are same")
-		sys.exit()
-	if id(child_fsm.node_list) == id(fsm.node_list):
-		print("node_list are same")
-		sys.exit()
 
 	child_fsm.fitness_score = 0
+	child_fsm.payoff_list.clear()
 	chance_to_mutate = .25
 	for node in child_fsm.node_list:	
 		chance = random.uniform(0, 1)
@@ -38,38 +38,46 @@ def make_child(fsm):
 #mutation functions
 # the same transition value but points to a different active state
 def change_node_action(node):
-	choice = random.choice(testGame.action_list)
+	choice = random.choice(game.action_list)
 	while choice == node.action:
-		choice = random.choice(testGame.action_list)
+		choice = random.choice(game.action_list)
 	node.action = choice
 
 # this is too aggressive -> cut balls off later
 def change_transition_destination(node):
-	max_change = len(testGame.action_list) # maximum number of allowed changes
+	max_change = len(game.action_list) # maximum number of allowed changes
 	for i in range(max_change):
 		chance = random.uniform(0, 1)
 		if chance <= (1/(i+1)):
-			transition_action = random.choice(testGame.action_list) # random so as to not "favor" lower indexes
+			transition_action = random.choice(game.action_list) # random so as to not "favor" lower indexes
 			# change the value for the above key in node's dictionary
 			node.transition_dict[transition_action] = random.randint(1, FSM.number_of_nodes) - 1
 		else: 
 			break # breaks if "chance" doesn't make the cutoff -> favors low numbers of changes
 
+# def noise(action):
+# 	noise_chance = 0.05
+# 	if random.uniform(0, 1) < noise_chance:
+# 		if 
+
 
 def evaluate(fsm): 
 	total_payoff = 0
-	for i in range(testGame.player_count): # might change testGame to be classless -> just a file with methods
-		testGame.opponent_last_action = testGame.default_action
+	for i in range(game.player_count): 
+		game.opponent_last_action = game.default_action
 		fsm.reset()
+		current_payoff = 0
 		for j in range(num_rounds):
 			# get player actions
-			player1_action = fsm.get_action()
-			player2_action = testGame.get_action(i)
-			player_actions = player1_action + player2_action
-			total_payoff += testGame.payoff[player_actions][0]
+			p1_action = fsm.get_action()
+			p2_action = game.get_action(i)
+			#player_actions = p1_action + p2_action
+			current_payoff += game.get_payoff(p1_action, p2_action)
 			# update information before next round
-			fsm.update_state(player2_action)
-			testGame.opponent_last_action = player1_action
+			fsm.update_state(p2_action)
+			game.opponent_last_action = p1_action
+		fsm.payoff_list.append(current_payoff)
+		total_payoff += current_payoff
 	
 	fsm.fitness_score = total_payoff
 
@@ -77,7 +85,7 @@ def evaluate(fsm):
 population = [] # list of all current individuals
 population_size = 50
 for x in range(population_size):
-	population.append(FSM(testGame.action_list))	
+	population.append(FSM(game.action_list))	
 
 for member in population:
 	evaluate(member)
@@ -101,8 +109,8 @@ while True:
 	else:
 		rounds_since += 1
 
-	if rounds_since >= 50:
-		break
+	# if rounds_since >= 50:
+	# 	break
 
 	# redfine num rounds for upcoming generation
 	next_generation = []
@@ -111,25 +119,46 @@ while True:
 	population.extend(next_generation)
 	population.sort(key=lambda x: x.fitness_score, reverse=True)
 	population = population[0:population_size]
-	if iterations > 5000:
+	if iterations > 500:
 		break
 	iterations += 1
-
-	
-
-for i, x in enumerate(population):
-	print(f"{i}: {x.fitness_score}")
 
 
 print(sum_total)
 print(starting)
 print(population[0].fitness_score)
 print(population[0].fitness_score - starting)
-print(iterations)
+print(rounds_since)
+print()
+print(population[0].payoff_list)
+print(len(population[0].payoff_list))
+print()
+for i, node in enumerate(population[0].node_list):
+	for j in game.action_list:
+		print(f"{i} -> {node.transition_dict[j]} [label = \"{node.action}/{j}\"];")
 
 
-# fsm = FSM(game)
-# print(fsm.node_list[0].transition_dict)
+print(population[0].payoff_list)
+print(population[0].fitness_score)
+
+
+
+
 # print()
-# change_transition_destination(fsm.node_list[0])
-# print(fsm.node_list[0].transition_dict)
+# fsm = population[0]
+# fsm.reset()
+# current_payoff = 0
+# for i in range(num_rounds):
+# 	player1_action = fsm.get_action()
+# 	player2_action = game.get_action(2)
+# 	player_actions = player1_action + player2_action
+# 	current_payoff += game.payoff[player_actions][0]
+# 	print(f"{i}: {player_actions}")
+# 	print(f"{i}: {game.payoff[player_actions][0]}")
+# 	print()
+# 	# update information before next round
+# 	fsm.update_state(player2_action)
+# 	game.opponent_last_action = player1_action
+
+
+# print(current_payoff)
